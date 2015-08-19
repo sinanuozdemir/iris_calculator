04a6bb6ab7dc99dc6572a8393cad59a6c9d2b784
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2015-08-18 23:15:40 -0700

    still iris?

--- controller.py
+++ controller.py
@@ -1,5 +1,9 @@
-
-from flask import Flask, render_template
+import operator
+import numpy as np
+import json
+import sqlite3
+import ff_work
+from flask import Flask, render_template, g, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import IntegerField, StringField, SubmitField, SelectField, DecimalField
@@ -9,132 +13,133 @@ from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_iris
 import pickle
 
-
-
 #Initialize Flask App
 app = Flask(__name__)
+dict_of_df = pickle.load(open('nfl_model.pkl', 'r'))
+names = []
+for pos, df in dict_of_df.items():
+	names += list(df['Name']+' / ' + df['Pos'])
+names = [(n, n) for n in names]
 
-#Initialize Form Class
-# This form will take in the form data on the front end and use it to predict
-# using a pre-loaded model in the model folder!!!!
-class PredictForm(Form):
-	sepal_length = DecimalField('Sepal Length (cm):', places=2, validators=[Required()])
-	sepal_width = DecimalField('Sepal Width (cm):', places=2, validators=[Required()])
-	petal_length = DecimalField('Petal Length (cm):', places=2, validators=[Required()])
-	petal_width = DecimalField('Petal Width (cm):', places=2, validators=[Required()])
+def get_db():
+	DATABASE = 'kickass_db.db'
+	db = getattr(g, 'db', None)
+	if db is None:
+		db = g.db = sqlite3.connect(DATABASE)
+	return db
+
+def getUpToDate():
+	dict_of_df = pickle.load(open('nfl_model.pkl', 'r'))
+	cur = get_db().cursor()
+	for name, pos, mine in cur.execute(''' select * from pick '''):
+		print name, pos, mine
+		ff_work.removePlayerFromPos(dict_of_df, name, pos)
+	return ff_work.calculateImportances(dict_of_df)
+
+
+
+def init_db():
+	db = get_db()
+	cur = db.cursor()
+	cur.execute('''DROP TABLE pick''')
+	cur.execute('''CREATE TABLE pick (name text, pos text, mine text)''')
+
+
+def insert(table, fields=(), values=()):
+	cur = get_db().cursor()
+	query = 'INSERT INTO %s (%s) VALUES (%s)' % (
+		table,
+		', '.join(fields),
+		', '.join(['?'] * len(values))
+	)
+	cur.execute(query, values)
+	g.db.commit()
+	id = cur.lastrowid
+	cur.close()
+	return id
+
+def display(dict_of_df):
+	db = get_db()
+	cur = get_db().cursor()
+	mine = cur.execute('''select * from pick where mine="t" ''').fetchall()
+	mine = [{'pos':m[1], 'name':m[0].title()} for m in mine]
+	allowed = {'QB':2, 'TE':4, 'WR':5, 'DEF':1, 'K':1, 'RB':4}
+	left = allowed.copy()
+	chosen = {'QB':0, 'TE':0, 'WR':0, 'DEF':0, 'K':0, 'RB':0}
+	for m in mine:
+		left[m['pos']] -= 1
+		chosen[m['pos']] += 1
+	picks = {}
+	urgency = {}
+	for pos in ['QB', 'RB', 'WR', 'TE', 'K', 'DEF']:
+		if left[pos] > 0:
+			short = dict_of_df[pos].sort_index(by = 'distance', ascending = False).head(7)
+			std_ = np.std(list(dict_of_df[pos].sort_index(by = 'distance', ascending = False).head(10)['distance']))
+			urgency[pos] = round(std_ * (1 - (chosen[pos] / float(allowed[pos]))), 2)
+			picks[pos] = list(short['Name'] + ' / ' + short['distance'].astype(str))
+	picks['mine'] = mine
+	picks['left'] = left
+	picks['chosen'] = chosen
+	picks['urgency'] = urgency
+	picks['top_field'] = max(urgency.iteritems(), key=operator.itemgetter(1))[0]
+	picks['top_pick'] = picks[max(urgency.iteritems(), key=operator.itemgetter(1))[0]][0].split(' / ')[0]
+	return picks
+
+
+
+class PickForm(Form):
+	name = SelectField('Name', choices=names)
 	submit = SubmitField('Submit')
+	mine = SelectField('Name', choices=[('f', 'NOT MINE'), ('t', 'MINE')])
+
+
 
 
 # this will take in the form data on the front end and train the model and store it in the model folder!
 class TrainForm(Form):
-	n_neighb = SelectField('Number of Neighbors:', choices=[(2,2),(3,3),(4,4),(5,5),(6,6)],coerce=int)
-	param_2 = DecimalField('Parameter 2 (Optional)', places = 2, validators=[Optional()])
-	param_3 = DecimalField('Parameter 3 (Optional)', places = 2, validators=[Optional()])
-	param_4 = DecimalField('Parameter 4 (Optional)', places = 2, validators=[Optional()])
+	write = DecimalField('write any number in here to refresh', places=2, validators=[Required()])
 	submit = SubmitField('Submit')
+
+
 
 
 @app.route('/',methods=['GET', 'POST'])
 def model():
-	
-	prediction, sepal_length, sepal_width, petal_length, petal_width, n_neighb  = None, None, None, None, None, None
+
+	dict_of_df = getUpToDate()
+
 	train_form = TrainForm(csrf_enabled=False)
-	predict_form = PredictForm(csrf_enabled=False)
-
-
-
-													######################
-													# Training The Model #
-													######################
+	pick_form = PickForm(csrf_enabled=False)
+	picks = display(dict_of_df)
 
 	if train_form.validate_on_submit():
-
+		init_db()
 		# store the submitted values
 		submitted_data = train_form.data
-		print submitted_data
+		# ff_work.gatherStats()
+		dict_of_df = pickle.load(open('nfl_model_backup.pkl', 'r'))
 
 
-		#Retrieve values from form
-		n_neighb = submitted_data['n_neighb']
-		param_2 = submitted_data['param_2']
-		param_3 = submitted_data['param_3']
-		param_4 = submitted_data['param_4']
-		# notice I'm using the same names I used in my TrainForm class up there!!!! On line 29
+		return redirect('/')
 
+	
 
-														######
-														# ML #
-														######
-		#Load Iris Data
-		iris_data = load_iris()
-		features = iris_data.data
-		feature_names = iris_data.feature_names
-		target = iris_data.target
-		target_names = iris_data.target_names
-
-
-		knn = KNeighborsClassifier(n_neighbors = n_neighb)    # replace with your own ML model here!!!!!
-		knn.fit(features, target)
-
-													############
-													# Pickling #
-													############
-		with open('model/my_model.pkl', 'wb') as f:           
-			pickle.dump(knn, f)
-
-
-
-
-		print "Trained"										  # output to tell us we are done
-
-
-
-
-
-
-													##############
-													# Predicting #
-													##############
-
-	elif predict_form.validate_on_submit():
-
+	elif pick_form.validate_on_submit():
+		db = get_db()
+		cur = db.cursor()
 		# store the submitted values
-		submitted_data = predict_form.data
-		print submitted_data
-
-		#Retrieve values from form
-		sepal_length = submitted_data['sepal_length']
-		sepal_width = submitted_data['sepal_width']
-		petal_length = submitted_data['petal_length']
-		petal_width = submitted_data['petal_width']
-		# notice I'm using the same names I used in my PredictForm class up there!!!! On line 20
-
-		#Create array from values
-		flower_instance = [sepal_length, sepal_width, petal_length, petal_width]
-
-		# unpickle my model
-		knn = pickle.load(open('model/my_model.pkl'))
-
-		# the machine learning part!!!
-		my_prediction = knn.predict(flower_instance)
-
-
-		# need to get the target_names again!
-		iris_data = load_iris()
-		target_names = iris_data.target_names
-		# so our output is pretty
-
-		# Return only the Predicted iris species
-		prediction = target_names[my_prediction][0].capitalize()
-		# This variable ends up getting put in bold on the front end!
-		# When you insert your own ML model, make sure your output is called "prediction"
+		submitted_data = pick_form.data
+		name, pos = submitted_data['name'].split(' / ')
+		mine = submitted_data.get('mine', 'f')
+		insert('pick', ['name', 'pos', 'mine'], [name.lower(), pos, mine])
+		return redirect('/')
 
 
 	return render_template(
 		'model.html',
-		predict_form=predict_form, 
-		train_form = train_form, 
-		prediction=prediction)
+		pick_form=pick_form, 
+		train_form = train_form,
+		picks = picks)
 
 
 
