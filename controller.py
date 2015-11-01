0e4f661e316f2888dfbeadce60cbf0628a326916
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2015-10-31 21:36:28 -0700

    Asd

--- controller.py
+++ controller.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, g, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
-from flask.ext.wtf import Form
-from wtforms import IntegerField, StringField, SubmitField, SelectField, DecimalField
-from wtforms.validators import Required, Optional
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import request
 
@@ -11,20 +8,6 @@ application = Flask(__name__)
 application.config.from_object('config')
 db = SQLAlchemy(application)
 import models
-
-
-def insert_(table, fields=(), values=()):
-	cur = db.cursor()
-	query = 'INSERT INTO %s (%s) VALUES (%s)' % (
-		table,
-		', '.join(fields),
-		', '.join(['?'] * len(values))
-	)
-	cur.execute(query, values)
-	g.db.commit()
-	id_ = cur.lastrowid
-	cur.close()
-	return id_
 
 
 @application.route('/insert', methods=['POST'])
@@ -38,16 +21,7 @@ def insert():
 
 @application.route('/test',methods=['GET', 'POST'])
 def model():
-	print "hello"
-	# print db.session.query(models.Visit).count()
-	return render_template(
-		'model.html'
-		)
-
-
-
-
-
+	return render_template('model.html')
 
 #Handle Bad Requests
 @application.errorhandler(404)
