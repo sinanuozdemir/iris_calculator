6ed2f5ca917f648c4cee7938424f58aa1b0d590c
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2015-11-05 20:02:33 -0800

    Asd

--- controller.py
+++ controller.py
@@ -56,10 +56,11 @@ def insert():
 			d['browser'] = user_agent.browser.family
 			d['is_bot'], d['is_mobile'], d['is_tablet'], d['is_pc'] = user_agent.is_bot, user_agent.is_mobile, user_agent.is_tablet, user_agent.is_pc
 		d['full_url'] = request.environ.get('HTTP_REFERER')
-		r = website_re.match(d['full_url'])
-		d['base'] = r.group(3)+r.group(4)
-		d['after'] = r.group(5)
-		d['gets'] = r.group(6)
+		ur = d['full_url'].replace('https://','').replace('http://','').replace('www.','')
+		if '/' not in ur: ur += '/'
+		d['base'], d['after'] = ur[:ur.index('/')], ur[ur.index('/'):]
+		if '?' in ur:
+			d['gets'] = ur.split('?')[1]
 		d['secure'] = 'https://' in d['full_url']
 		print d
 		p = models.Visit(**d)
