6ef937858c551495db190588987304304a491dee
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2015-12-12 15:51:33 -0800

    asd

--- controller.py
+++ controller.py
@@ -417,14 +417,13 @@ def getNotifications():
 	appid = getModel(models.App, appid = getAppIDForEmail(request.args['email'])).id
 	emails = [d.id for d in db.session.query(models.Email).filter_by(app_id = appid).all()]
 	links = [d.id for d in db.session.query(models.Link).filter_by(app_id = appid).all()]
-	emails = db.session.query(models.Visit.state, models.Visit.country, models.Visit.date).filter(models.Visit.email_id.in_(emails)).filter_by(notified=False).all()
-	links = db.session.query(models.Visit.state, models.Visit.country, models.Visit.date).filter(models.Visit.link_id.in_(links)).filter_by(notified=False).all()
+	emails = db.session.query(models.Visit).filter(models.Visit.email_id.in_(emails)).filter_by(notified=False).all()
+	links = db.session.query(models.Visit).filter(models.Visit.link_id.in_(links)).filter_by(notified=False).all()
 	n_e, n_l = [], []
 	for e in emails:
-		d = e.__dict__
-		del d['_labels']
-		# d['minutes_ago'] = int((datetime.utcnow() - d['date']).total_seconds()/60)
-		del d['date']
+		d = {}
+		# d = {'date':e.date}
+		d['minutes_ago'] = int((datetime.utcnow() - e.date).total_seconds()/60)
 		n_e.append(d)
 	# for l in links:
 	# 	d = l.__dict__
