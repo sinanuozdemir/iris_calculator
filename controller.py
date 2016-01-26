20f312aea7178beb3684504f20a76173962cc26e
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2016-01-26 13:04:38 -0800

    asd

--- controller.py
+++ controller.py
@@ -689,6 +689,7 @@ def cadenceInfo():
 	except:
 		return jsonify(fuck="off")
 	dates = {}
+	now = datetime.utcnow()
 	emails = db.session.query(models.Email).filter(models.Email.emailid.in_(emailids)).all()
 	emails = [(e.id, e.date_sent) for e in emails if e.id and e.date_sent]
 	for e in emails:
@@ -698,19 +699,19 @@ def cadenceInfo():
 	ids = [a[0] for a in emails]
 	num_emails = float(len(ids))
 	all_opens = db.session.query(models.Visit).filter(models.Visit.email_id.in_(ids)).all()
-	most_recent_opens = [(s.email.to_address, datetime.strftime(s.date, '%m/%d/%Y'), s.email.subject) for s in sorted(all_opens, key = lambda x:x.date)[-10:]]
+	most_recent_opens = [(s.email.to_address, ((now-s.date).seconds), ((now-s.date).seconds/60), ((now-s.date).seconds/3600), s.email.subject) for s in sorted(all_opens, key = lambda x:x.date)[-10:]][::-1]
 	all_opens = {a.email_id:1 for a in all_opens}
 	
 
 	all_links = db.session.query(models.Link).filter(models.Link.email_id.in_(ids)).all()
 	link_ids = [l.id for l in all_links]
 	all_clicks = db.session.query(models.Visit).filter(models.Visit.link_id.in_(link_ids)).all()
-	most_recent_clicks = [(s.link.email.to_address, datetime.strftime(s.date, '%m/%d/%Y'), s.link.email.subject) for s in sorted(all_clicks, key = lambda x:x.date)[-10:]]
+	most_recent_clicks = [(s.link.email.to_address, ((now-s.date).seconds), ((now-s.date).seconds/60), ((now-s.date).seconds/3600), s.link.email.subject) for s in sorted(all_clicks, key = lambda x:x.date)[-10:]][::-1]
 	all_clicks = {a.link.email_id:1 for a in all_clicks}
 	
 
 	all_replies = db.session.query(models.Email).filter(and_(models.Email.replied_to.in_(ids), models.Email.bounce==False))
-	most_recent_replies = [(s.from_address, datetime.strftime(s.date_sent, '%m/%d/%Y'), s.subject) for s in sorted(all_replies, key = lambda x:x.date_sent)[-10:]]
+	most_recent_replies = [(s.from_address, ((now-s.date_sent).seconds), ((now-s.date_sent).seconds/60), ((now-s.date_sent).seconds/3600), s.subject) for s in sorted(all_replies, key = lambda x:x.date_sent)[-10:]][::-1]
 	all_replies = {e.replied_to:1 for e in all_replies}
 	# print most_recent_replies, most_recent_opens, most_recent_clicks
 	
@@ -727,6 +728,7 @@ def cadenceInfo():
 			'replies':sum([all_replies.get(a, 0) for a in dates[_day]]), 
 			'clicks':sum([all_clicks.get(a, 0) for a in dates[_day]]),
 		}
+	stats['dates'] = {x:stats['dates'][x] for x in sorted(stats['dates'])}
 	stats['most_recent_replies'] = most_recent_replies
 	stats['most_recent_clicks'] = most_recent_clicks
 	stats['most_recent_opens'] = most_recent_opens
